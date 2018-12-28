354297e26dae0b5b2e911561c7e41fc16efd5cda
Motoki Kimura <motoki.kimura.1990@gmail.com>
2018-12-28 13:54:03 +0000

    Add Dockerfile

---
+++ docker/exec.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Exec
+CONTAINER="mrcnn"
+
+nvidia-docker exec -it ${CONTAINER} /bin/bash
+
---
+++ docker/run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Set image name
+IMAGE="mrcnn:latest"
+if [ $# -eq 1 ]; then
+    IMAGE=$1
+fi
+
+# Set project root dicrectory to map to docker 
+THIS_DIR=$(cd $(dirname $0); pwd)
+PROJ_DIR=`dirname ${THIS_DIR}`
+
+# Make some directories if not exist
+#mkdir -p ${PROJ_DIR}/data ${PROJ_DIR}/models
+
+# Run container
+CONTAINER="mrcnn"
+
+nvidia-docker run -it --rm --ipc=host \
+	-p 8888:8888 -p 6006:6006 \
+	-v ${PROJ_DIR}:/work \
+	--name ${CONTAINER} \
+	${IMAGE}
